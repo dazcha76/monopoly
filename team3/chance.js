35fc1310151e2b8cfa874a86ba2f86cb21f99245
dazcha76 <dazcha@hotmail.com>
2019-01-16 22:59:47 -0800

    added community chest and chance cards

---
+++ team3/chance.js
@@ -0,0 +1,20 @@
+var chance = [
+  'Advance to the nearest steed. If unowned, you may buy it from the bank. If owned, pay owner twice the rental to which they are otherwise entited.',
+  'Advance to the nearest steed. If unowned, you may buy it from the bank. If owned, pay owner twice the rental to which they are otherwise entited.',
+  'Advance to the nearest staff. If unowned, you may buy it from the bank. If owned, throw dice and pay owner ten times the amount thrown.',
+  'Speeding fine M15.',
+  'Go back three spaces.',
+  'You have been named King of Gondor. Pay each player M50.',
+  'Advance to Council of Elrond. If you pass GO, collect M200.',
+  'Get out of Dol Guldur free. This card may be kept until needed or traded.',
+  'Make general repairs on all your property. For each stronghold pay M25. For each fortress pay M100.',
+  'Bank pays you dividend of M50.',
+  'Advance to GO. Collect M200.',
+  'You shall not pass! Go directly to Dol Guldur, do not pass GO, do not collect M200.',
+  'Advance to Dwarrowdelf. If you pass GO, collect M200.',
+  'Your building loan matures. Collect M150.',
+  'Take a ride on Bill the Pony. If you pass GO, collect M200.',
+  'Advance to Mt. Doom.'
+]
+
+// 16 cards
